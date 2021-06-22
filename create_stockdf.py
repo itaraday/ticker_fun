19ab1d470af6097d7cdba002cd855bdb871bd01a
itaraday <ira.taraday.01@gmail.com>
2021-06-22 17:00:04 -0400

    if nan name use description with minimal cleaning

--- create_stockdf.py
+++ create_stockdf.py
@@ -24,22 +24,24 @@ if __name__ == '__main__':
     #todo add OTC?
     # http://eoddata.com/
     data_lst = ["TSX", "NYSE", "NASDAQ", "OTCBB"]
-    stock_df = pd.DataFrame
+    df_lst = pd.DataFrame
     for data in data_lst:
         df = pd.read_csv('data\{}.txt'.format(data), delimiter="\t")
         df.dropna(inplace=True)
         df["market"] = data
-        if not stock_df.empty:
-            stock_df = stock_df.append(df)
-        else:
-            stock_df = df
+        df_lst.append(df)
 
+    stock_df = pd.concat(df_lst)
+    del df_lst
     stock_df['Name'] = stock_df['Description'].str.lower()
     stock_df['Name'] = stock_df['Description'].str.replace('.com','')
     stock_df['Name'] = stock_df['Name'].str.replace('[^\w\s]','')
     stock_df['Name'] = stock_df['Name'].apply(lambda x: cleanco(x).clean_name())
+    stock_df['Name'].fillna(stock_df['Description'], inplace=True)
+    stock_df['Name'] = stock_df['Name'].str.lower()
+
     stock_df.loc[stock_df["Symbol"]=="CRSR", "Name"] = 'Corsair'
     stock_df.loc[stock_df["Symbol"] == "LOGI", "Name"] = 'Logitech'
     stock_df.loc[stock_df["Symbol"] == "DIS", "Name"] = 'Disney'
-    stock_df.to_csv("data/stock_df.csv", index=False)
+    stock_df.to_csv("data/stock_df2.csv", index=False)
     print("done")

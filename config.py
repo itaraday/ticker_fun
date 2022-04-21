0c6d0dbc525386cd26f9d9dfc3dbd3ca25b28096
itaraday <ira.taraday.01@gmail.com>
2022-04-21 13:14:18 -0400

    pull reddit by a specific date

--- config.py
+++ config.py
@@ -3,17 +3,16 @@ import json
 
 vcap = None
 
-def get_creds():
+def get_creds(env="dev"):
     global vcap
     if not vcap:
         LOCAL_DIR = os.path.dirname(__file__)
-        config_file = os.path.join(LOCAL_DIR, "config.json")
+        config_file = os.path.join(LOCAL_DIR, f"config_{env}.json")
         if os.path.isfile(config_file):
             with open(config_file) as f:
                 vcap = json.load(f)
         else:
             raise RuntimeError("missing config")
-        print("woot")
         return vcap
     else:
         return vcap
--- reddit_pull.py
+++ reddit_pull.py
@@ -1,9 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
 import praw
+from psaw import PushshiftAPI
 from tqdm import tqdm
 from string import punctuation
 from config import get_creds
+import traceback
+import argparse
 
 
 creds = get_creds()
@@ -12,22 +15,33 @@ reddit = praw.Reddit(
         client_secret=creds["reddit_client_secret"],
         user_agent=creds["reddit_user_agent"]
 )
-today = datetime.today().strftime("%Y-%m-%d")
+api = PushshiftAPI(reddit)
 
 
-def get_ticker(subreddits, limit=100):
+def check_available(post):
+    try:
+        good = (post.removed_by_category is None) and post.is_robot_indexable
+    except Exception as e:
+        good = (post.author is not None) and (post.author.name.lower is not "automoderator")
+    return good
+
+
+def get_ticker(subreddits, start_dt, end_dt, limit=100):
     comments_lst = []
     threads_lst = []
 
     #"+".join(subreddits) would pull the top X across all subs in list
     subreddits = "+".join(subreddits)
-    submissions = list(reddit.subreddit(subreddits).top("day", limit=limit))
+    submissions = list(api.search_submissions(subreddit=subreddits, before=end_dt, after=start_dt,
+                                         limit=limit, sort_type="score"))
+    #submissions = list(reddit.subreddit(subreddits).top("day", limit=limit))
     # limit = 3
     # counter = 0
     for submission in tqdm(submissions, position=1, leave=False, desc="submissions: {}".format(subreddits)):
+        if not check_available(submission):
+            continue
         thread = {}
         thread["id"] = submission.id
-        # title = submission.title
         thread["title"] = submission.title
         thread["date"] = datetime.fromtimestamp(submission.created).strftime("%Y-%m-%d")
         thread["award_cnt"] = submission.total_awards_received
@@ -49,14 +63,20 @@ def get_ticker(subreddits, limit=100):
             thread["media"] = True
         thread["sub"] = submission.subreddit.display_name
         thread["url"] = "https://www.reddit.com{}".format(submission.permalink)
-        thread["author"] = submission.author.name
-        thread["author_karma"] = submission.author.awardee_karma
+        try:
+            thread["author"] = submission.author.name
+            thread["author_karma"] = submission.author.awardee_karma
+        except Exception as e:
+            #the thread is still active, but the author isn't ex: https://www.reddit.com/r/smallstreetbets/comments/rt9m0z/apple_calls_and_puts_this_week_mostly_puts/
+            continue
         # submission.comment_sort = "top"
         # submission.comment_limit = 100
         submission.comments.replace_more(limit=0)
         comments = submission.comments
         threads_lst.append(thread)
         for comment in tqdm(comments, position=1, leave=False, desc="comments: {}".format(subreddits)):
+            if not check_available(comment):
+                continue
             comment_data = {}
             try:
                 if comment.stickied:
@@ -78,22 +98,46 @@ def get_ticker(subreddits, limit=100):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--date", help="date to run service for in %Y-%m-%d")
+    args = parser.parse_args()
+
+
     subreddits = ["stocks", "investing", "smallstreetbets", "pennystocks", "weedstocks", "StockMarket",
                   "Trading", "Daytrading", "wallstreetbets"]
     thread_df_all = pd.DataFrame
     comment_df_all = pd.DataFrame
-    for subreddit in tqdm(subreddits, position=0):
-        print(subreddit)
-        limit = 20
-        if subreddit == "wallstreetbets":
-            limit *= 2
-        thread_df, comment_df = get_ticker([subreddit], limit=limit)
-        if not thread_df_all.empty:
-            thread_df_all = thread_df_all.append(thread_df)
-            comment_df_all = comment_df_all.append(comment_df)
-        else:
-            thread_df_all = thread_df
-            comment_df_all = comment_df
-    thread_df_all.to_csv("data/thread_df-{}.csv".format(today), index=False)
-    comment_df_all.to_csv("data/comment_df-{}.csv".format(today), index=False)
 
+    if args.date:
+        try:
+            print(f"pulling for {args.date}")
+            my_date = datetime.strptime(args.date, '%Y-%m-%d')
+        except Exception as e:
+            print("date should be in %Y-%m-%d format")
+            exit(2)
+    else:
+        print(f"pulling for {datetime.today()}")
+        my_date = datetime.today()
+    start_dt = int((my_date-timedelta(days=1)).timestamp())
+    end_dt = int(my_date.timestamp())
+
+    try:
+        for subreddit in tqdm(subreddits, position=0):
+            print(subreddit)
+            limit = 10
+            if subreddit == "wallstreetbets":
+                limit *= 2
+            thread_df, comment_df = get_ticker([subreddit], start_dt, end_dt, limit=limit)
+            if not thread_df_all.empty:
+                thread_df_all = thread_df_all.append(thread_df)
+                comment_df_all = comment_df_all.append(comment_df)
+            else:
+                thread_df_all = thread_df
+                comment_df_all = comment_df
+    except Exception as e:
+        trace = traceback.format_exc()
+        print("Uncaught exception: {}\n{}".format(e, trace))
+        exit(1)
+    run_date = datetime.utcfromtimestamp(start_dt).strftime("%Y-%m-%d")
+    thread_df_all.to_csv("data/thread_df-{}.csv".format(run_date), index=False)
+    comment_df_all.to_csv("data/comment_df-{}.csv".format(run_date), index=False)
